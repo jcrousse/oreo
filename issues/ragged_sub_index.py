b1f0cef9f91e303169fa2995f99e351228f6060a
John Crousse <jcrousse@gmail.com>
2019-12-30 17:32:04 +0100

    trying to compile into a Keras model, but issues with RaggedTensors again.. Next step: see todo

--- issues/ragged_sub_index.py
+++ issues/ragged_sub_index.py
@@ -3,43 +3,65 @@ applying a model on a specific dimentions of a ragged tensor
 """
 
 import tensorflow as tf
+from data.dataset_prep import TextDataSetPrep
+
+VOCAB_N_L0 = 5
+EMBEDDING_SIZE_L0 = 2
+ENCODING_SIZE_L1 = 4
+
 
 ragged_simple_seq = tf.ragged.constant([
     ['First', 'observation', 'single', 'sentence'],
     ['Second', 'observation', 'another', 'sentence']])
 
+ds = TextDataSetPrep(nrows=1).read_tfr_dataset("train_small.tfr")
 
-# Look up the embedding for each word.
-word_buckets = tf.strings.to_hash_bucket_fast(ragged_simple_seq, 5)
-embedded = tf.keras.layers.Embedding(5, 2)(word_buckets)
-sentence_encoding = tf.keras.layers.LSTM(4)(embedded)
-print(sentence_encoding)
+ts_list = []
+len_l0 = []
+len_l1 = []
+for item in ds.take(2):
+    ts_list.append(item[0]['characters'])
+    len_l0.append(item[0]['len_level_0'])
+    len_l1.append(item[0]['len_level_1'])
 
 
-ragged_nested_seq = tf.ragged.constant([
-    [['First_observation', 'sentence1'],
-     ['First_observation', 'sentence2']],
-    [
-        ['Second_observation', 'another_sentence']
-    ]])
 
-# ragged_nested_seq = tf.ragged.constant([
-#     [[['o1_s1_w1_c1', 'o1_s1_w1_c2'], ['o1_s1_w2_c1']],
-#      [['o1_s2_w1_c1'], ['o1_s2_w2_c1']]],
-#     [
-#         [['o2_s1_w1_c1'], ['o2_s1_w2_c1']]
-#     ]])
+input_tokens = tf.keras.layers.Input(shape=(None,), name='characters', dtype=tf.string)
+input_grouping = tf.keras.layers.Input(shape=(None,), name='len_level_0', dtype=tf.int32)
 
-word_buckets_nested = tf.strings.to_hash_bucket_fast(ragged_nested_seq, 5)
-embedded_nested = tf.keras.layers.Embedding(5, 4)(word_buckets_nested)
+word_buckets = tf.strings.to_hash_bucket_fast(input_tokens, VOCAB_N_L0)
+embedded = tf.keras.layers.Embedding(VOCAB_N_L0, EMBEDDING_SIZE_L0)(word_buckets)
 
-print(embedded_nested)
-print(embedded_nested.bounding_shape())
-print(embedded_nested.flat_values)
-reshaped_ragged = tf.RaggedTensor.from_row_lengths(
-    values=embedded_nested.flat_values,
-    row_lengths=[2, 2, 2]  # words per sentence
+reshaped = tf.RaggedTensor.from_row_lengths(
+    values=embedded,
+    row_lengths=tf.reshape(input_grouping, [-1]))
+# yet another situation where the combination of Keras and RaggedTensor sucks.
+# todo next: reshape without ragged (use padding), and try to compile,
+#  then try to print out the tf.reshape(input_grouping, [-1] to check if it is the value we want.
+#  if hardcoding the same value as the reshaped tensor works, then we can raise an issue. 
+encoded = tf.keras.layers.LSTM(ENCODING_SIZE_L1)(reshaped)
+
+model = tf.keras.Model(inputs=[input_tokens, input_grouping], outputs=encoded)
+model({'characters': ts_list[0], 'len_level_0': len_l0[0]})
+model.predict(ds.take(2))
+
+ragged_by_char = tf.ragged.constant(ts_list)
+word_buckets = tf.strings.to_hash_bucket_fast(ragged_by_char, VOCAB_N_L0)
+embedded = tf.keras.layers.Embedding(VOCAB_N_L0, EMBEDDING_SIZE_L0)(word_buckets)
+
+regrouped = tf.RaggedTensor.from_row_lengths(
+    values=embedded,
+    row_lengths=len_l0  # words per sentence
 )
+
+sentence_encoding = tf.keras.layers.LSTM(ENCODING_SIZE_L1)(embedded)
+
+
+
+
+
+
+
 print(reshaped_ragged)
 sentence_encoding_nested = tf.keras.layers.LSTM(4)(reshaped_ragged)
 print(sentence_encoding_nested)
@@ -51,3 +73,4 @@ print(reshaped_by_obs)
 level_2_seq = tf.keras.layers.LSTM(4)(reshaped_by_obs)
 
 print(level_2_seq)
+# todo: make the above for arbitrary N nested levels and user selected sequence model
